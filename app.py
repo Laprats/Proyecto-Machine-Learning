d6a59691dc717da813f5f92a0ef2b2eb19cb6c7f
Laprats <166111147+Laprats@users.noreply.github.com>
2025-01-22 12:10:30 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,33 +1,30 @@
 import streamlit as st
 import pickle
-import numpy as np
+import pandas as pd
 
-with open('model.pkl', 'rb') as file:
-    model = pickle.load(file)
+with open('linear_model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
 
-with open('scaler.pkl', 'rb') as file:
-    scaler = pickle.load(file)
-
-
-st.title("Predicción de Aceptación de Producto Bancario")
-st.write("""
-Ingrese los datos del cliente para predecir si aceptará o no el producto.
-""")
+with open('scaler.pkl', 'rb') as scaler_file:
+    scaler = pickle.load(scaler_file)
 
 
-age = st.number_input("Edad del cliente:", min_value=18, max_value=100, step=1)
-balance = st.number_input("Balance promedio en la cuenta bancaria:")
-campaign = st.number_input("Número de contactos realizados durante la campaña actual:")
-pdays = st.number_input("Días desde el último contacto con el cliente (si aplica):", min_value=-1, step=1)
-previous = st.number_input("Número de contactos previos exitosos:")
+st.title('Predicción del Gasto Anual de clientes de una Tienda de Ropa (Regresión Lineal)')
+
+length_of_membership = st.number_input('Length of Membership (meses)', min_value=0)
+time_on_app = st.number_input('Time on App (minutos)', min_value=0)
+avg_session_length = st.number_input('Avg. Session Length (minutos)', min_value=0)
+
+user_data = pd.DataFrame({
+    'Length of Membership': [length_of_membership],
+    'Time on App': [time_on_app],
+    'Avg. Session Length': [avg_session_length]
+})
 
 
-if st.button("Predecir"):
-   
-    input_data = np.array([[age, balance, campaign, pdays, previous]])
-    input_data_scaled = scaler.transform(input_data)
-   
-    prediction = model.predict(input_data_scaled)
-    resultado = "Aceptará el producto" if prediction[0] == 1 else "No aceptará el producto"
-    
-    st.subheader(f"Resultado de la predicción: {resultado}")
+user_data_standardized = scaler.transform(user_data)
+
+
+prediction = model.predict(user_data_standardized)
+
+st.write(f'Predicción del Gasto Anual: {prediction[0]:.2f}')
